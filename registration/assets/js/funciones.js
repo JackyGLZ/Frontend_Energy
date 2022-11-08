e4875b5f40748ab5abfb72378892bb82c0088b2a
Oswaldo <53319294+Oswaldob0411@users.noreply.github.com>
2022-11-07 21:59:53 -0500

    Add files via upload

---
+++ registration/assets/js/funciones.js
@@ -0,0 +1,19 @@
+(function () {
+  // Obtener todos los formularios a los que queremos aplicar estilos de validación de Bootstrap personalizados
+  var forms = document.querySelectorAll('.needs-validation')
+  //Recorremos los forms y evitamos en envío sin validacion
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {            
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }else{
+          alert('FORM VALIDADO')
+        }
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
+     
+
